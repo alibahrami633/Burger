66fa29d12e0a34f813e6af0306d6165f8bda9059
Aliagho <ali.bahrami633@yahoo.com>
2020-05-11 22:28:31 +1000

    added burger controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,31 @@
+//Model is the business layer
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+const dbName = "burgers";
+
+let burgerController = {
+    all: (cb) => {
+        orm.all(dbName, (res) => {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: (cols, vals, cb) => {
+        orm.create(dbName, cols, vals, (res) => {
+            cb(res);
+        });
+    },
+    update: (objColVals, condition, cb) => {
+        orm.update(dbName, objColVals, condition, (res) => {
+            cb(res);
+        });
+    },
+    delete: (condition, cb) => {
+        orm.delete(dbName, condition, (res) => {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burgerController;
