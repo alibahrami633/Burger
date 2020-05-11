8b3c908242e2c7263c2cd455c30e1209c9d5a996
Aliagho <ali.bahrami633@yahoo.com>
2020-05-11 22:23:26 +1000

    converted ORM code to ES6

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,5 @@
 // Import MySQL connection.
-var connection = require("../config/connection.js");
+const connection = require("../config/connection.js");
 
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
@@ -7,9 +7,9 @@ var connection = require("../config/connection.js");
 // The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
-    var arr = [];
+    let arr = [];
 
-    for (var i = 0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
         arr.push("?");
     }
 
@@ -18,11 +18,11 @@ function printQuestionMarks(num) {
 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
-    var arr = [];
+    let arr = [];
 
     // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-        var value = ob[key];
+    for (let key in ob) {
+        let value = ob[key];
         // check to skip hidden properties
         if (Object.hasOwnProperty.call(ob, key)) {
             // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
@@ -40,18 +40,18 @@ function objToSql(ob) {
 }
 
 // Object for all our SQL statement functions.
-var orm = {
-    all: function (tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function (err, result) {
+let orm = {
+    all: (tableInput, cb) => {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
             if (err) {
                 throw err;
             }
             cb(result);
         });
     },
-    create: function (table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
+    create: (table, cols, vals, cb) => {
+        let queryString = "INSERT INTO " + table;
 
         queryString += " (";
         queryString += cols.toString();
@@ -62,17 +62,16 @@ var orm = {
 
         console.log(queryString);
 
-        connection.query(queryString, vals, function (err, result) {
+        connection.query(queryString, vals, (err, result) => {
             if (err) {
                 throw err;
             }
-
             cb(result);
         });
     },
     // An example of objColVals would be {name: panther, sleepy: true}
-    update: function (table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
+    update: (table, objColVals, condition, cb) => {
+        let queryString = "UPDATE " + table;
 
         queryString += " SET ";
         queryString += objToSql(objColVals);
@@ -80,20 +79,19 @@ var orm = {
         queryString += condition;
 
         console.log(queryString);
-        connection.query(queryString, function (err, result) {
+        connection.query(queryString, (err, result) => {
             if (err) {
                 throw err;
             }
-
             cb(result);
         });
     },
-    delete: function (table, condition, cb) {
-        var queryString = "DELETE FROM " + table;
+    delete: (table, condition, cb) => {
+        let queryString = "DELETE FROM " + table;
         queryString += " WHERE ";
         queryString += condition;
 
-        connection.query(queryString, function (err, result) {
+        connection.query(queryString, (err, result) => {
             if (err) {
                 throw err;
             }
