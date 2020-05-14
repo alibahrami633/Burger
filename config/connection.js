04dd63c0b3f092d99b3aa1ef68df7e8f5da64798
Aliagho <ali.bahrami633@yahoo.com>
2020-05-14 13:03:22 +1000

    trying another database

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,26 @@
-// Set up MySQL connection.
-const mysql = require("mysql");
-require("dotenv").config();
+let mysql = require("mysql");
+let connection;
 
-/** This object represents the default database configuration */
-const config = {
-    port: process.env.MYSQL_PORT,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-};
-
-
-/** Check for process.env.JAWSDB_URL first since this app is hosted on heroku */
-const connection = mysql.createConnection(process.env.JAWSDB_URL || config);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else {
+  connection = mysql.createConnection({
+    host: "sql12.freemysqlhosting.net",
+    port: 3306,
+    user: "sql12340418",
+    password: "VbwfEAUwJW",
+    database: "sql12340418",
+  });
+}
 
 // Make connection.
-connection.connect((err) => {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
